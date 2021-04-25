0914d9675bb11de1df44f3c13c8782c96e4374ab
koteitan <ukaibutton@mac.com>
2021-04-25 22:07:18 +0900

    copied from GoogolMaps2.0 rev c51f2144811

---
+++ version.js
@@ -0,0 +1,5 @@
+document.getElementById("versiondisplay").innerHTML = "0.01";
+document.getElementById("changelog").innerHTML = 
+"<table align='right'><tr><td style='text-align:right'>20XX/XX/XX XX:XX JST</td><td>&nbsp;</td><td>started coding.</td></tr></table>";
+document.getElementById("knownbugs").innerHTML = 
+"<table align='right'><tr><td style='text-align:right'>under construction.</td></tr></table>";
